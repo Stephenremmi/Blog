9059a2237830ac59ff3ae467b9ed324892939542
stephen remmi <stephenremmi@gmail.com>
2020-06-15 11:16:26 +0300

    Initial Commit

---
+++ config.py
@@ -0,0 +1,33 @@
+import os 
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://remmi:stephen@localhost/bloglist"
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
+
+    # email configurations
+    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_LSS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://remmi:stephen@localhost/blog_test"
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://remmi:stephen@localhost/bloglist"
+    DEBUG = True
+
+
+config_options = {
+    "development": DevConfig,
+    "production": ProdConfig,
+    "test": TestConfig
+}
